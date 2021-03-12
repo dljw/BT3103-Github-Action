45242eb9a73497962f05923b46a9a029a13f3fc8
dljw <darrenljw96@gmail.com>
2021-03-13 00:53:07 +0800

    first commit

---
+++ src/components/charts/BarChart.vue
@@ -0,0 +1,25 @@
+<template >
+    <div>
+        <div style="background:blue; color:white; font-size:200%"><h1>Bar Chart</h1></div>
+        <button v-on:counter="click1"> CLICK ME</button>
+        <chart></chart>
+    </div>
+</template>
+
+<script>
+import Chart from "./BarChart.js";
+export default {
+  components: {
+    Chart
+  },
+  data(){
+    return{
+      ok:false
+    }
+  }
+};
+</script>
+
+<style scoped>
+
+</style>
